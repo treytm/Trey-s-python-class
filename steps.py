6a45066a0c138e22ca69098bb4bb45ff6b4172c7
treytm <89431838+treytm@users.noreply.github.com>
2021-09-11 21:18:27 -0400

    Create steps.py

---
+++ steps.py
@@ -0,0 +1,14 @@
+# Named constants - create one for each month to store number of days in that month; assume this is NOT a leap year
+
+def main():
+    # Open the steps file using the first command line argument to get the input file name. For example: python steps.py steps.txt would open the file steps.txt to read the steps 
+
+    # Display the average steps for each month using a function to calculate and display
+
+    # Close the file.
+
+def average_steps(steps_file, month_name, days):
+    # compute the average number of steps for the given month
+    # output the results
+    
+main()
